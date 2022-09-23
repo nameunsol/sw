d108f85b4dbb9b2c71b1fe5cf036ec6b328f44c6
남은솔 <106670648+nameunsol@users.noreply.github.com>
2022-09-23 09:38:50 +0900

    1

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+from tkinter import Y
+
+
+def add(x,y):
+    a=x+y
+    return a
+
+
+
+def print_hello():
+    print("Hello World!!")
+
+
+print("남은솔 공주 웅냥냥")
+def print_KIT():
+    print("kumoh")
+    print("KIT")
+if __name__ == '__main__':
+    print_hello()
+    print_KIT()
+    print(print_hello(1,2))
+
+
