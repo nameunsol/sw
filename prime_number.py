57efb4dea74be3fee25081237fee2f6682612f7c
남은솔 <106670648+nameunsol@users.noreply.github.com>
2022-09-23 10:07:07 +0900

    2

--- prime_number.py
+++ prime_number.py
@@ -1,7 +1,10 @@
 def is_prime_number(x):
-    for num in range(2, x):
-        if x % num == 0:
-            print("The number {num1} is not a prime number.".format(num1=x))
-            return
+    if x<=1:
+        print("error ")
+    else:
+        for num in range(2, x):
+            if x % num == 0:
+                print("The number {num1} is not a prime number.".format(num1=x))
+           
     print("The number {num1} is a prime number.".format(num1=x))
 
