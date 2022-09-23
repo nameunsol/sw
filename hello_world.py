4342dd8867b89d76907502df59e5044d5292bfe0
남은솔 <106670648+nameunsol@users.noreply.github.com>
2022-09-23 09:25:54 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,23 @@
+from tkinter import Y
+
+
+def add(x,y):
+    a=x+y
+    return a
+
+
+
 def print_hello():
     print("Hello World!!")
 
-if __name__ == '__main__':
-    print_hello()
 
 print("남은솔 공주 웅냥냥")
-def print_KTT():
+def print_KIT():
     print("kumoh")
-    
+    print("KIT")
+if __name__ == '__main__':
+    print_hello()
+    print_KIT()
+    print(print_hello(1,2))
+
+
